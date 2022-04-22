6f790cd0018f2d6ace0f00e45c13784169ef7639
Hehoku <hehoku@outlook.com>
2022-04-22 14:57:06 +0800

    add: repetition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import Sketch from 'react-p5'
-import { setup, draw } from './demos/robot'
+import { setup, draw } from './demos/repetition'
 
 function App () {
   return <Sketch setup={setup} draw={draw} />
