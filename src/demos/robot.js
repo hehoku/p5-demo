2b6dbd4f22cd0c9e4dc740bf57c06d2557d538ac
Hehoku <hehoku@outlook.com>
2022-04-28 21:25:19 +0800

    update: robot auto random move

--- src/demos/robot.js
+++ src/demos/robot.js
@@ -1,17 +1,11 @@
 import { w, h } from '../utils/constants'
 
-let x = 60
-const y = h / 2
-let bodyHeight = 180
-let neckHeight = 40
+let x = 180
+let y = 400
+let bodyHeight = 153
+let neckHeight = 56
 const radius = 45
-let img
-
-function preload (p) {
-  // path relative to /public, use `./logo512.png` for local files
-  img = p.loadImage('./logo512.png')
-  console.log(img)
-}
+let angle = 0.0
 
 const setup = (p, canvasParentRef) => {
   p.createCanvas(w, h).parent(canvasParentRef)
@@ -21,49 +15,49 @@ const setup = (p, canvasParentRef) => {
 }
 
 const draw = p => {
+  // change position by a small random amount
+  x += p.random(-4, 4)
+  y += p.random(-1, 1)
+
+  // change height of neck
+  neckHeight = 80 + p.sin(angle) * 30
+  angle += 0.05
+
   p.clear()
   p.background(29, 215, 95)
-  p.image(img, x, y, radius * 2, radius * 2)
 
   // text
   p.textSize(28)
   p.text('on small step for man ...', 25, 60)
 
-  let ny = -1 * (bodyHeight + neckHeight + radius)
-  p.translate(p.mouseX, y)
-  if (p.mouseIsPressed) {
-    p.scale(1.0)
-  } else {
-    p.scale(0.6)
-  }
+  let ny = y - 1 * (bodyHeight + neckHeight + radius)
   // neck
   p.stroke(102)
-  p.line(12, -bodyHeight, 12, ny)
+  p.line(x + 2, y - bodyHeight, x + 2, ny)
+  p.line(x + 12, y - bodyHeight, x + 12, ny)
+  p.line(x + 22, y - bodyHeight, x + 22, ny)
 
-  // hair
-  p.push()
-  p.translate(12, ny)
-  let angle = -p.PI / 40.0
-  for (let i = 0; i < 40; i++) {
-    p.line(radius + 20, 0, 0, 0)
-    p.rotate(angle)
-  }
-  p.pop()
+  // antennae
+  p.line(x + 12, ny, x - 18, ny - 43)
+  p.line(x + 12, ny, x + 42, ny - 99)
+  p.line(x + 12, ny, x + 78, ny + 15)
 
   // body
   p.noStroke()
   p.fill(102)
-  p.ellipse(0, -33, 33, 33)
+  p.ellipse(x, y - 33, 33, 33)
   p.fill(0)
-  p.rect(-45, -bodyHeight, 90, bodyHeight - 33)
+  p.rect(x - 45, y - bodyHeight, 90, bodyHeight - 33)
+  p.fill(102)
+  p.rect(x - 45, y - bodyHeight + 17, 90, 6)
 
   // head
   p.fill(0)
-  p.ellipse(12, ny, radius, radius)
+  p.ellipse(x + 12, ny, radius, radius)
   p.fill(255)
-  p.ellipse(24, ny - 6, 14, 14)
+  p.ellipse(x + 24, ny - 6, 14, 14)
   p.fill(0)
-  p.ellipse(24, ny - 6, 3, 3)
+  p.ellipse(x + 24, ny - 6, 3, 3)
 }
 
-export { preload, setup, draw }
+export { setup, draw }
